69d22e714c37351764f776c13f17f1c0cb28bfc0
kagcaoili <kagcaoil@gmail.com>
2025-01-25 15:57:27 -0800

    Added collision detection so bubbles spawn without colliding with each other. When colliding with another bubble, spread the color

--- OveLrap/Assets/Scripts/WordBubble.cs
+++ OveLrap/Assets/Scripts/WordBubble.cs
@@ -10,9 +10,14 @@ public class WordBubble : MonoBehaviour
     public MeshRenderer renderer;
     public BubbleColor currentColor;
 
+    bool SetupDone = false;
+    //public bool IsColliding { get; private set; }
+    public bool IsColliding = false;
+
     public void SetWord(Word word) {
         this.word = word;
         text.text = word.word;
+        SetupDone = true;
     }
 
     public void SetColor(BubbleColor color) {
@@ -24,5 +29,49 @@ public class WordBubble : MonoBehaviour
     void Update() {
         transform.LookAt(Camera.main.transform);
         transform.Rotate(0, 180, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // if colliding with another bubble, set IsColliding to true
+        if (other.gameObject.GetComponent<WordBubble>() != null) {
+            IsColliding = true;
+            if(SetupDone) {
+                // only color change after word is set, this is to prevent color spread when first spawning bubbles
+                HandleColorChange(other.gameObject.GetComponent<WordBubble>());
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        // if colliding with another bubble, set IsColliding to true
+        if (other.gameObject.GetComponent<WordBubble>() != null) {
+            IsColliding = true;
+            if(SetupDone) {
+                // only color change after word is set, this is to prevent color spread when first spawning bubbles
+                HandleColorChange(other.gameObject.GetComponent<WordBubble>());
+            }
+        }
+    }
+
+    
+    private void OnTriggerExit(Collider other) {
+        // if no longer colliding with another bubble, set IsColliding to false
+        if (other.gameObject.GetComponent<WordBubble>() != null) {
+            IsColliding = false;
+        }
+    }
+    
+
+    private void HandleColorChange(WordBubble colliding) {
+        // if this bubble is already colored, do nothing. only non-colored bubbles will take the color.
+        if(currentColor.tone != BubbleTone.None) {
+            return;
+        }
+
+        // if this bubble is non-colored and is colliding with a bubble that already has a color, take its color
+        if(colliding.currentColor.tone != BubbleTone.None) {
+            Debug.Log("Color spread from " + colliding.word.word + " to " + word.word);
+            SetColor(colliding.currentColor);
+        }
     }
 }
