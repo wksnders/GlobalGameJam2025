273e3380f4b9b50c032a1bcfd3d9f73c563327a5
wksnders <42388072+wksnders@users.noreply.github.com>
2025-01-25 11:26:23 -0700

    TMP and Main menu skeleton code

---
+++ OveLrap/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+	public GameObject MainMenu;
+	public GameObject OptionsMenu;
+
+	public void playGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void options()
+	{
+		MainMenu.SetActive(false);
+		OptionsMenu.SetActive(true);
+	}
+
+	public void back()
+	{
+		MainMenu.SetActive(true);
+		OptionsMenu.SetActive(false);
+	}
+
+	public void exitGame()
+	{
+		Application.Quit();
+	}
+}
